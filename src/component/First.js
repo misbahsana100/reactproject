24a57a2ec86c2682702966d267e82368509f7728
uzma maryam <uzmamaryam48@gmail.com>
2021-10-31 12:11:09 +0500

    changes

--- src/component/First.js
+++ src/component/First.js
@@ -17,28 +17,46 @@
 // }
 // export default First
 
+// import {useState} from "react";
 
-import React,{useState} from "react";
-function First() {
-  const[items,setItems] = useState([])
-  const addItem =()=>{
-setItems([...items,{
-  id:items.length,
-  value:Math.floor(Math.random()*10) + 1
-}])
-  }
-  return(
-    <div>
-      <button onClick={addItem}>Add a number</button>
-      <ul>
-        {items.map(item=>(
-          <li key={item.id}>{item.value}</li>
-        ))}
-      </ul>
-    </div>
-  )
-}
-export default First
+import React from "react";
+const Todayclass =()=>{
+    const  [cities,setCities] = useState(['lahore','karachi','islamabad'])
+    return(
+        <div>
+      {
+          cities.map((item,index)=>(
+              <div key ={index}>{item}</div>
+    ))
+      }
+    
+
+
+</div>
+    )}
+    export default Todayclass;
+
+// import React,{useState} from "react";
+// function First() {
+//   const[items,setItems] = useState([])
+//   const addItem =()=>{
+// setItems([...items,{
+//   id:items.length,
+//   value:Math.floor(Math.random()*10) + 1
+// }])
+//   }
+//   return(
+//     <div>
+//       <button onClick={addItem}>Add a number</button>
+//       <ul>
+//         {items.map(item=>(
+//           <li key={item.id}>{item.value}</li>
+//         ))}
+//       </ul>
+//     </div>
+//   )
+// }
+// export default First
 
 // import React, { useState } from 'react';
 
