c93de65fdf96c7cfa9fb168192e1f133f0dedb0a
uzma maryam <uzmamaryam48@gmail.com>
2021-10-31 11:48:37 +0500

    .

---
+++ src/component/First.js
@@ -0,0 +1,58 @@
+// import React,{useState} from "react";
+// // import axios from 'axios'
+// const First =() => {
+//   const [isLit,setLit] =useState(true)
+//   let [num,setNum] = useState(0)
+//   return(
+//       <>
+//       The room is {isLit ? 'bright' : "dark"}
+//       <button onClick={()=>setLit(!isLit)}>On/Off</button>
+//       <h1>{num}</h1>
+//       <button onClick={()=>setNum(++num)}>+</button>
+//       <button onClick={()=>setNum(--num)}>-</button>
+
+
+//       </>
+//   )
+// }
+// export default First
+
+
+import React,{useState} from "react";
+function First() {
+  const[items,setItems] = useState([])
+  const addItem =()=>{
+setItems([...items,{
+  id:items.length,
+  value:Math.floor(Math.random()*10) + 1
+}])
+  }
+  return(
+    <div>
+      <button onClick={addItem}>Add a number</button>
+      <ul>
+        {items.map(item=>(
+          <li key={item.id}>{item.value}</li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+export default First
+
+// import React, { useState } from 'react';
+
+// function hook() {
+//   const [count, setCount] = useState(0);
+
+//   return (
+//     <div>
+//       <p>You clicked {count} times</p>
+//       <button onClick={() => setCount(count + 1)}>
+//         Click me
+//       </button>
+//     </div>
+//   );
+// }
+
+// export default hook
